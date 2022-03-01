ed03f98d694f0c16e514d5176a448841b9cafe5f
Júnior Matos <sk8.merp@gmail.com>
2022-03-01 16:49:29 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,14 @@
+class CpfGenerator{
+    static gerateNumber(){
+        let Numbers = '';
+        for(let i = Numbers.length; i < 11; i++){
+            Numbers = Numbers + (Math.floor(Math.random() * (9 - 0 + 1)) + 0);
+        }
+        return Numbers
+    }
+    static geratePointing(){
+        let NumGenerated = this.gerateNumber();
+        return NumGenerated.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+    }
+}
+console.log(CpfGenerator.geratePointing());
